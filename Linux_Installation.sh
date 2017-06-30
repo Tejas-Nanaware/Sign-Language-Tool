af68c52212444b829602b556f89864d2fa961ca2
Tejas Nanaware <Tejas-Nanaware@users.noreply.github.com>
2017-06-30 14:45:30 +0530

    Create Linux_Installation.sh

---
+++ Linux_Installation.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install python3 python git libopencv-dev python-pip python3-pip htop
+python3 -m pip install numpy matplotlib opencv-python
+python -m pip install numpy matplotlib opencv-python
