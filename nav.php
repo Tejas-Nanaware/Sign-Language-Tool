a254939d6b46aba8ced61d9841fccdae8b719638
Tejas-Nanaware <nanawaret@hotmail.com>
2017-07-01 02:00:04 +0530

    Fixed nav bar

--- nav.php
+++ nav.php
@@ -17,7 +17,7 @@
 	</head>
 	<body>
 	<!--Navbar-->
-		<nav class="navbar navbar-inverse">
+		<nav class="navbar navbar-inverse navbar-fixed-top">
 			<div class="container-fluid">
 				<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
