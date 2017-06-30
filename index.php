a23f5c3f301b4c59a073293b8ee7a908ed216391
Tejas-Nanaware <nanawaret@hotmail.com>
2017-07-01 00:23:32 +0530

    Updated Parallax

--- index.php
+++ index.php
@@ -16,6 +16,5 @@
 	<div style="background-color:red;font-size:36px" align="center">
 		<h3><span class="glyphicon glyphicon-wrench"></span> Page Under Construction</h3>
 	</div>
-	<div class="parallax"></div>
 	</body>
 </html>
